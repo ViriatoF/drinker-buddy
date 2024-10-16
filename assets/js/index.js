d8dd36e03bbad8c752c01aee51c8175428e0eec9
ViriatoF <viriato.ferreira44@gmail.com>
2024-10-16 15:26:46 +0200

    ♻️ Add dark/night button + JS associated, carousel style, neon style and delete pic no used

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,18 @@
-// Carousel code opening
-console.log("coucou")
-// Carousel code ending
+
 
 const mainArrow = document.getElementById("scroll_button");
 const arrow = document.querySelector("i");
 const buttonMain = document.querySelector("button");
+const nightDayButton = document.querySelector("#nightDayMode");
+const body = document.querySelector("body");
+let darkMode = false;
 
 arrow.addEventListener("mouseover", ()=>{
   arrow.classList.add("arrow_color");
 })
 
 arrow.addEventListener("mouseleave", ()=>{
+  console.log("couleur");
   arrow.classList.remove("arrow_color");
 })
 
@@ -24,5 +26,27 @@ function scrollToNav() {
   });
 };
 
-
 mainArrow.addEventListener("click", scrollToNav);
+
+function switchDark(){
+  body.classList.add("night_mode_body");
+  nightDayButton.textContent = "Light";
+  darkMode = true;
+  console.log(darkMode);
+  
+}
+
+function switchLight(){
+  body.classList.remove("night_mode_body");
+  nightDayButton.textContent = "Dark";
+  darkMode = false;
+  console.log(darkMode);
+}
+
+
+nightDayButton.addEventListener("click",switchDark);
+
+if(darkMode==true){
+
+  nightDayButton.addEventListener("click",switchLight);
+}
